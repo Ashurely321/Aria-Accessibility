0e2f01108bac3b4aab4955535706fdb8fd431b10
Ashley Harris <laylen24@gmail.com>
2025-09-23 22:21:53 -0500

    Add paragraphs explaining aria and web accessibility

--- main.js
+++ main.js
@@ -10,13 +10,17 @@ const passwordInput = document.getElementById("password");
 const birthdateInput = document.getElementById("birthdate");
 
 // Event Listener
-mainForm.addEventListener('submit', function (event) {
+mainForm.addEventListener("submit", function (event) {
   // Prevent the action from the form to refresh the page
   event.preventDefault();
 
   // Get selected Interests
   const selectedInterests = [];
-  document.querySelectorAll('input[name="interests"]:checked').forEach(checkbox => {selectedInterests.push(checkbox.value)});
+  document
+    .querySelectorAll('input[name="interests"]:checked')
+    .forEach((checkbox) => {
+      selectedInterests.push(checkbox.value);
+    });
 
   // Get the output
   outputDiv.innerHTML = `
@@ -25,16 +29,33 @@ mainForm.addEventListener('submit', function (event) {
     <p>My email is: ${emailInput.value}</p>
     <p>My password is: ${passwordInput.value}</p>
     <p>My birthday is: ${birthdateInput.value}</p>
-    <p>My interests are: ${selectedInterests.join(', ')}</p>
-  `
+    <p>My interests are: ${selectedInterests.join(", ")}</p>
+  `;
 
-  outputDiv.style.display = 'block';
+  //aria button accessibility
+  const ariaButton = document.getElementById("aria-button");
+
+  ariaButton.addEventListener("click", () => {
+    //action when clicked
+    window.location.href = "https://www.example.com";
+  });
+
+  ariaButton.addEventListener("keydown", (event) => {
+    if (event.key === "Enter" || event.key === "") {
+      event.preventDefault(); //to prevent scrolling on space
+      ariaButton.click();
+    }
+  });
+
+  outputDiv.style.display = "block";
 
   // Clear the inputs
-  firstNameInput.value = '';
-  lastNameInput.value = '';
-  emailInput.value = '';
-  passwordInput.value = '';
-  birthdateInput.value = '';
-  document.querySelectorAll('input[name="interests"]').forEach(checkbox => {checkbox.checked = false;})
+  firstNameInput.value = "";
+  lastNameInput.value = "";
+  emailInput.value = "";
+  passwordInput.value = "";
+  birthdateInput.value = "";
+  document.querySelectorAll('input[name="interests"]').forEach((checkbox) => {
+    checkbox.checked = false;
+  });
 });
